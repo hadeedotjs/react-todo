8b99db995db92562a053e3ac7bd95cd84691d1ae
Hadii <135013774+hadirasool46@users.noreply.github.com>
2024-12-27 15:55:24 +0530

    start

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,17 @@ const app = express()
 app.use(express.json())
 
 
-app.get()
+app.post("/todo", (req,res)=>{
+    
+})
+
+app.get("/todos", (req,res)=>{
+    
+})
+
+app.put("/mark", (req,res)=>{
+
+})
 
 
 app.listen(3000)
